ee29c47a0f85770c8a1ba88ce7b924a26fdff1e3
shanME <giorgie103188@gmail.com>
2024-08-19 18:23:09 +0400

    CALC DISPLAY SUMMARY

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ const displayMovements = (movements) => {
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__value">${mov}</div>
+          <div class="movements__value">${mov}€</div>
         </div>
     `;
 
@@ -86,10 +86,31 @@ displayMovements(account1.movements);
 //CALC DISPLAY BALANCE
 const calcDisplayBalance = (movements) => {
   const balance = movements.reduce((acc, cur) => acc + cur, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance}€`;
 };
 
 calcDisplayBalance(account1.movements);
+
+//CALC DISPLAY SUMMARY
+const calcDisplaySummary = (movements) => {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, cur) => acc + cur, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, cur) => acc + cur, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .reduce((acc, cur) => acc + cur, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+calcDisplaySummary(account1.movements);
 
 // USERNAME
 const createUsernames = (accs) => {
